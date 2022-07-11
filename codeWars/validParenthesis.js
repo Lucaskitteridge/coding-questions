8d91b4ed63c9f4d61e5e946af53b4bf55ee5c139
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-07-11 10:30:18 -0400

    valid parenthesis question added

---
+++ codeWars/validParenthesis.js
@@ -0,0 +1,21 @@
+/* DESCRIPTION:
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+*/
+
+const validParen = string => {
+
+}
+
+
+console.log(validParen("(")) // false
+console.log(validParen(")(()))" )) // false
+console.log(validParen("()")) // true
+console.log(validParen("(())((()())())")) // true
+
+
