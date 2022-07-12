9d95f645bcb4b92f2a24b34751c83f8c2491c674
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-07-12 10:34:50 -0400

    solve of customer lines

--- codeWars/supermarketQueue.js
+++ codeWars/supermarketQueue.js
@@ -9,17 +9,32 @@ n: a positive integer, the number of checkout tills.
 */
 
 const queueTime = function (customers, tills) {
-  
+  //If one till just reduce numbers
+  if (tills === 1 && customers.length > 0) {
+    return customers.reduce((prev, curr) => prev + curr);
+  }
+  //create a virtual version: two lines with a zero as the sum of each of their waittimes
+  const arr = [];
+  for (let x = 0; x < tills; x++) {
+    arr.push(0);
+  }
+  //for each customer, look for the shortest line and then add this customers time to the shortest
+  for (let i = 0; i < customers.length; i++) {
+    const min = arr.indexOf(Math.min(...arr));
+    arr[min] += customers[i];
+  }
+  //return the largest value in the array of times
+  return Math.max(...arr);
 };
 
-queueTime([5, 3, 4], 1);
+console.log(queueTime([5, 3, 4], 1));
 // should return 12
 // because when there is 1 till, the total time is just the sum of the times
 
-queueTime([10, 2, 3, 3], 2);
+console.log(queueTime([10, 2, 3, 3], 2));
 // should return 10
 // because here n=2 and the 2nd, 3rd, and 4th people in the
 // queue finish before the 1st person has finished.
 
-queueTime([2, 3, 10], 2);
+console.log(queueTime([2, 3, 10], 2));
 // should return 12
